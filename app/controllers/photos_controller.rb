9b7d41280812bad20ea6be68286ebc044c99bf37
a-ruizdel <a-rdl@wireless-s1-no-150-18-36.uchicago.edu>
2015-07-28 17:32:31 -0500

    Completes HWK 5 for Photogram_phase_1

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -14,7 +14,11 @@ class PhotosController < ApplicationController
 def destroy
   Photo.find(params["id"]).destroy
   redirect_to :back
+end
 
+def new_form
+
+  render ("photos/new.html.erb")
 end
 
 end
